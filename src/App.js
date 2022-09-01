8c71000b3a88bf70f29bcc8bcd5806003689bc75
TateDeVito <tatedevito@Tates-MBP.lan>
2022-09-01 12:40:27 -0400

    Simple component structure for an answer display added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import PersistentDrawerLeft from './Components/PersistentDrawerLeft.tsx';
+import AnswerDisplay from './Components/AnswerDisplay.tsx';
 
 const answersCompleted = ["Sign-Up Form", "Item Cart", "Recipe", "Blog", "Rainbow Circle", "Navbar"]
+const answerToDisplay = "SignUpForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <PersistentDrawerLeft answers={answersCompleted} />
+        <AnswerDisplay />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome! This React app is used to track progress in the AlgoExpert.io FrontEnd Expert course.
---
+++ src/Components/HTML/SignUpForm.tsx
@@ -0,0 +1,55 @@
+// 10/10 tests passed
+// Task:
+// Implement a sign up form with the correct labels and inputs, and a Sign Up button that allows you
+// to submit the form:
+
+// Keys:
+//  - Understand that within a form, you can make certain fields "required"
+//  - You can also add label tags around inputs to add a label to those fields
+
+import './SignUpForm.css';
+
+const signUpForm =
+<div>
+    <div>
+      <h1>Sign Up Form</h1>
+      <p><i>Implement a sign up form with the correct labels and inputs, and a Sign Up button.</i></p>
+      <h2 className='test-pass'> 10/10 tests passed </h2>
+      <h3>Keys</h3>
+      <ul>
+        <li>Understand that within a form, you can make certain fields "required"</li>
+        <li>You can also add label tags around inputs to add a label to those fields</li>
+      </ul>
+    </div>
+<form style={{ backgroundColor: "lightgrey"}}>
+  <div>
+    <label>Username: 
+      <input type="text" required />  
+    </label> 
+    <label>Password: 
+      <input type="password" required />
+    </label> 
+    <label>Email: 
+      <input type="email" required />
+    </label> 
+  </div>
+  <div>
+    <label>Phone Number: 
+      <input type="tel" />  
+    </label> 
+    <label>Date of Birth: 
+      <input type="date" />
+    </label> 
+    <label>I agree to the TOS 
+      <input type="checkbox" required/>
+    </label> 
+  </div>
+  <div>
+    <button value="Sign Up" type="submit">Sign Up</button>
+  </div>
+    </form>
+    </div>
+
+export default function SignUpForm() {
+  return signUpForm;
+}
