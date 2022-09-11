2bea6a4efb1a4d6a5d9674dab0c19d3372189d14
TateDeVito <tatedevito19@gmail.com>
2022-09-10 21:29:51 -0400

    Card added to sign up form

--- src/Components/HTML/SignUpForm.tsx
+++ src/Components/HTML/SignUpForm.tsx
@@ -7,49 +7,55 @@
 //  - Understand that within a form, you can make certain fields "required"
 //  - You can also add label tags around inputs to add a label to those fields
 
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 import './SignUpForm.css';
 
 const signUpForm =
-<div>
+  <>
+    <h2>Sign Up Form</h2>
+    <Card variant='outlined' sx={{ minWidth: 275, height: 620, marginBottom: 20 }}>
+      <CardContent>
+      <div>
+        <h3>Task</h3>
+        <p><i>Implement a sign up form with the correct labels and inputs, and a Sign Up button.</i></p>
+        <h3>Keys</h3>
+        <ul>
+          <li>Understand that within a form, you can make certain fields "required"</li>
+          <li>You can also add label tags around inputs to add a label to those fields</li>
+        </ul>
+      </div>
+      <h3>HTML Answer Component</h3>
+  <form style={{ backgroundColor: "lightgrey"}}>
     <div>
-      <h1>Sign Up Form</h1>
-      <p><i>Implement a sign up form with the correct labels and inputs, and a Sign Up button.</i></p>
-      <h3>Keys</h3>
-      <ul>
-        <li>Understand that within a form, you can make certain fields "required"</li>
-        <li>You can also add label tags around inputs to add a label to those fields</li>
-      </ul>
+      <label>Username: 
+        <input type="text" required />  
+      </label> 
+      <label>Password: 
+        <input type="password" required />
+      </label> 
+      <label>Email: 
+        <input type="email" required />
+      </label> 
     </div>
-    <h3>HTML Answer Component</h3>
-<form style={{ backgroundColor: "lightgrey"}}>
-  <div>
-    <label>Username: 
-      <input type="text" required />  
-    </label> 
-    <label>Password: 
-      <input type="password" required />
-    </label> 
-    <label>Email: 
-      <input type="email" required />
-    </label> 
-  </div>
-  <div>
-    <label>Phone Number: 
-      <input type="tel" />  
-    </label> 
-    <label>Date of Birth: 
-      <input type="date" />
-    </label> 
-    <label>I agree to the TOS 
-      <input type="checkbox" required/>
-    </label> 
-  </div>
-  <div>
-    <button value="Sign Up" type="submit">Sign Up</button>
-  </div>
-    </form>
-      <h2 className='test-pass'> 10/10 tests passed </h2>
-    </div>
+    <div>
+      <label>Phone Number: 
+        <input type="tel" />  
+      </label> 
+      <label>Date of Birth: 
+        <input type="date" />
+      </label> 
+      <label>I agree to the TOS 
+        <input type="checkbox" required/>
+      </label> 
+        </div>
+        <div>
+          <button value="Sign Up" type="submit">Sign Up</button>
+        </div>
+      </form>
+    </CardContent>
+    </Card>
+  </>
 
 export default function SignUpForm() {
   return signUpForm;
